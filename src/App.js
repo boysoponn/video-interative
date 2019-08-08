16d29a4763ff5054eac5b428be34f63cdef4bf1d
boysopon <soponcmm@gmail.com>
2019-08-08 23:44:01 +0700

    gg

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import './app.css';
 import Homepage from './components/homepage';
 import Interactive from './components/interactive';
 import "../node_modules/video-react/dist/video-react.css";
-import { BrowserRouter as  Router, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends React.Component {
 
   render(){
       return (
-    <div>
-      <Router> 
-        <div> 
-          <Route exact path="/" component={Homepage} />       
-          <Route path="/interactive" component={Interactive} />          
-        </div>
-      </Router>
-    </div>
+        <BrowserRouter  basename="/oth/money-happiness-center/">
+          <div>
+            <Switch>
+              <Route exact path="/" component={Homepage} />       
+              <Route path="/interactive" component={Interactive} />  
+            </Switch>
+          </div>
+        </BrowserRouter>
     
   );
   }
